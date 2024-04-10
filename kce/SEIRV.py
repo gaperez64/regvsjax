87d8a000e0938488343bc411b9e929643721e5f6
Guillermo A. Perez <gaperez64@gmail.com>
2024-04-10 17:00:09 +0200

    almost to the point of graphing, though the vaccinated compartment goes negative

--- kce/SEIRV.py
+++ kce/SEIRV.py
@@ -10,10 +10,13 @@ class Model:
         self.contact = jnp.asarray(df.values)
         # mortality rates
         df = pd.read_csv("data/rateMor_eurostat_2021.csv")
+        df.drop(df.tail(1).index, inplace=True)  # FIXME
         self.dailyMort = jnp.asarray(df["Value"].values) / 365
         # initial population
         df = pd.read_csv("data/refPop_eurostat_2021.csv")
-        self.initPop = jnp.asarray(df["Population"].values)
+        df.drop(df.tail(1).index, inplace=True)  # FIXME
+        self.initPop = jnp.asarray(df["Population"].values,
+                                   dtype=float)
         # TODO: vaccination efficacy
         # other parameters
         self.q = 1.8 / 15.2153
@@ -22,9 +25,17 @@ class Model:
         self.omegaImm = 0.33 / 365
         self.omegaVacc = 0.33 / 365
         self.seedInf = 200
-        self.seedAge = list(range(5, 51))
+        self.seedAges = jnp.asarray(range(5, 51))
         self.delta = 0.7
         self.peak = 1  # day of the year (out of 365)
+
+    def init(self):
+        S = self.initPop
+        E = jnp.zeros(S.size)
+        Inf = jnp.zeros(S.size)
+        R = jnp.zeros(S.size)
+        V = jnp.zeros(S.size)
+        return (S, E, Inf, R, V)
 
     # Using Forward Newton
     def step(self, S, E, Inf, R, V, day):
@@ -55,11 +66,13 @@ class Model:
         newV = V - V2S - V2D
         return (newS, newE, newInf, newR, newV, day + 1)
 
-    def age(self, S, E, I, R, V, day):
+    def seedInfs(self, S, E, Inf, R, V, day):
+        newS = S.at[self.seedAges].add(-self.seedInf)
+        newE = E.at[self.seedAges].add(self.seedInf)
+        return (newS, newE, Inf, R, V, day)
+
+    def vaccinate(self, S, E, Inf, R, V, day):
         pass
 
-    def infect(self, S, E, I, R, V, day):
-        pass
-
-    def vaccinate(self, S, E, I, R, V, day):
+    def age(self, S, E, Inf, R, V, day):
         pass
--- test.py
+++ test.py
@@ -1,7 +1,44 @@
+import pandas as pd
+import seaborn as sns
+
 from kce.SEIRV import Model
 
 
-if __name__ == "__main__":
-    print("Testing the model")
+def simulate():
     m = Model()
+    (S, E, Inf, R, V) = m.init()
+    day = 279  # 9 * 31 ~ End of September
+    state = (S, E, Inf, R, V, day)
+    state = m.seedInfs(*state)
+    trajectories = [state]
+    for _ in range(365):
+        state = m.step(*state)
+        trajectories.append(state)
+    return trajectories
+
+
+def plot(trajectories):
+    summd = []
+    for (S, E, Inf, R, V, day) in trajectories:
+        entry = ("Susceptible", S.sum(), day)
+        summd.append(entry)
+        entry = ("Exposed", E.sum(), day)
+        summd.append(entry)
+        entry = ("Infectious", Inf.sum(), day)
+        summd.append(entry)
+        entry = ("Recovered", R.sum(), day)
+        summd.append(entry)
+        entry = ("Vaccinated", V.sum(), day)
+        summd.append(entry)
+    df = pd.DataFrame(summd, columns=["Compartment", "Population", "Day"])
+    print(df)
+    sns.relplot(
+        data=df, x="Day", y="Population"
+    )
+
+
+if __name__ == "__main__":
+    ts = simulate()
+    print("Done with simulations, trajectories acquired!")
+    plot(ts)
     exit(0)
