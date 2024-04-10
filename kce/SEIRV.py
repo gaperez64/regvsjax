31a3d430b694d68e944bf8ad2684b4995af49aa3
Guillermo A. Perez <gaperez64@gmail.com>
2024-04-10 11:30:12 +0200

    have initial pop now

--- kce/SEIRV.py
+++ kce/SEIRV.py
@@ -1,4 +1,5 @@
 import jax.numpy as jnp
+import numpy as np
 import pandas as pd
 
 
@@ -9,21 +10,50 @@ class Model:
         self.contact = jnp.asarray(df.values)
         # mortality rates
         df = pd.read_csv("data/rateMor_eurostat_2021.csv")
-        self.rateMor = jnp.asarray(df["Value"].values)
+        self.dailyMort = jnp.asarray(df["Value"].values) / 365
+        # initial population
+        df = pd.read_csv("data/refPop_eurostat_2021.csv")
+        self.initPop = jnp.asarray(df["Population"].values)
+        # TODO: vaccination efficacy
         # other parameters
         self.q = 1.8 / 15.2153
         self.sigma = 0.5
         self.gamma = 0.5
-        self.omega_i = 0.9 / 365
-        self.omega_v = 0.9 / 365
-        self.mu_d = self.rateMor / 365
+        self.omegaImm = 0.33 / 365
+        self.omegaVacc = 0.33 / 365
         self.seedInf = 200
         self.seedAge = list(range(5, 51))
-        self.alpha = 0.2
-        self.delta = 0.7  # FIXME
+        self.delta = 0.7
+        self.peak = 1  # day of the year (out of 365)
 
-    def step(self, S, E, I, R, V, day):
-        pass
+    # Using Forward Newton
+    def step(self, S, E, Inf, R, V, day):
+        z = 1 + jnp.sin(2 * np.pi * (day - self.peak) / 365)
+        beta = self.contact * self.q
+        force = z * jnp.dot(beta, Inf)
+
+        # daily transitions
+        S2E = S * force  # element-wise product
+        E2I = E * self.sigma
+        I2R = Inf * self.gamma
+        R2S = R * self.omegaImm
+        V2S = V * self.omegaVacc
+
+        # daily mortality = element-wise product with mortality
+        # rates
+        S2D = S * self.dailyMort
+        E2D = E * self.dailyMort
+        I2D = Inf * self.dailyMort
+        R2D = R * self.dailyMort
+        V2D = R * self.dailyMort
+
+        # new values for all components
+        newS = S - S2E + R2S + V2S - S2D
+        newE = E + S2E - E2I - E2D
+        newInf = Inf + E2I - I2R - I2D
+        newR = R + I2R - R2S - R2D
+        newV = V - V2S - V2D
+        return (newS, newE, newInf, newR, newV, day + 1)
 
     def age(self, S, E, I, R, V, day):
         pass
