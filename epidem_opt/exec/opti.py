b873ea9099faa10546a25ff0c22fdead1656e82a
Kasper Engelen <kasper.engelen@hotmail.com>
2025-02-06 23:18:43 +0100

    Added regression for gradient.

--- epidem_opt/exec/opti.py
+++ epidem_opt/exec/opti.py
@@ -1,9 +1,13 @@
 import configparser
+import pickle
 from datetime import date, timedelta
 import jax
+from jax import numpy as jnp
 
 from epidem_opt.src.kce.epidata import EpiData
-from epidem_opt.src import kce as epistep
+# from epidem_opt.src import kce as epistep
+import epidem_opt.src.kce.epistep as epistep
+
 
 
 # jax.config.update("jax_enable_x64", True)
@@ -55,6 +59,12 @@ def getCost(vaccRates, m, state, endDate):
     return total_cost
 
 
+def check_output(ref_cost, actual_cost, ref_grad, actual_grad):
+    assert ref_cost == actual_cost, "Error, cost mismatch"
+    assert jnp.allclose(ref_grad, actual_grad), "Error, grad mismatch"
+    print("All correct.")
+
+
 if __name__ == "__main__":
     m = EpiData()
     config = configparser.ConfigParser()
@@ -63,6 +73,17 @@ if __name__ == "__main__":
     grad_cost = jax.grad(getCost)
     cost = getCost(m.vaccRates, m, m.startState(),  endDate)
     print(f"The price of it all = {cost}")
-    print(grad_cost(m.vaccRates, m, m.startState(), endDate))
+    # with open("./working_dir/reference_cost.pickle", 'wb') as ref_file:
+    #     pickle.dump(cost, ref_file)
+    grad_cost = grad_cost(m.vaccRates, m, m.startState(), endDate)
+    print(grad_cost)
+    # with open("./working_dir/reference_grad.pickle", 'wb') as ref_file:
+    #     pickle.dump(grad_cost, ref_file)
     # TODO: do gradient descent with the gradient.
+
+    with open("./working_dir/reference_cost.pickle", 'rb') as ref_cost_file:
+        ref_cost = pickle.load(ref_cost_file)
+    with open("./working_dir/reference_grad.pickle", 'rb') as ref_grad_file:
+        ref_grad = pickle.load(ref_grad_file)
+    check_output(ref_cost=ref_cost, actual_cost=cost, ref_grad=ref_grad, actual_grad=grad_cost)
     exit(0)
